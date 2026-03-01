e79b262a3824552c6d414a1a3e1c51937c14dbc1
Callum <callumery11@outlook.com>
2026-03-01 06:08:08 +0000

    Started work on the Extraction Endpoint. Included new tests to check hashing functionality.

--- src/main/java/com/w/callum/pdf_service_data/util/ImageHashingGoLangBindings.java
+++
@@ -1,7 +0,0 @@
-package com.w.callum.pdf_service_data.util;
-
-import com.sun.jna.*;
-
-public interface ImageHashingGoLangBindings extends Library {
-    String hashPageOfDocumentString(String cStr);
-}
--- src/test/java/com/w/callum/pdf_service_data/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.w.callum.pdf_service_data;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
