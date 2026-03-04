44be0245cce78f7186680f88a945a96901d91d01
Callum <callumery11@outlook.com>
2026-03-04 16:16:37 +0000

    Working version of the ExtractionTextStripper.java

---
+++ src/test/java/com/w/callum/pdf_service_data/ExtractionTest.java
@@ -0,0 +1,42 @@
+package com.w.callum.pdf_service_data;
+
+import com.w.callum.pdf_service_data.extraction.ExtractionTextStripper;
+import com.w.callum.pdf_service_data.model.Coordinate;
+import com.w.callum.pdf_service_data.util.ImageHashing;
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.PDFRenderer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Base64;
+
+public class ExtractionTest {
+    @Test
+    public void ExtractionTestOne() {
+        File encodedDocument = new File("src/test/java/com/w/callum/pdf_service_data/invoice_Phillip Breyer_11502.txt");
+        try (FileInputStream fileInputStream = new FileInputStream(encodedDocument)) {
+            byte[] data = fileInputStream.readAllBytes();
+            String encodedData = new String(data, StandardCharsets.UTF_8);
+            byte[] decodedDocument = Base64.getDecoder().decode(encodedData);
+
+            try (PDDocument document = Loader.loadPDF(decodedDocument)) {
+                Coordinate coordinate = new Coordinate(0, document.getPage(0).getMediaBox().getWidth(), 0, document.getPage(0).getMediaBox().getHeight());
+                try (ExtractionTextStripper extractionTextStripper = new ExtractionTextStripper(coordinate)) {
+                    extractionTextStripper.setPageStart("0");
+                    extractionTextStripper.setPageEnd("0");
+                    extractionTextStripper.getText(document);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
